abb8bac32638562860b43974f5b9ae9f7df011eb
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-11-22 21:54:34 -0400

    Update form

--- src/components/FormEpi/index.jsx
+++ src/components/FormEpi/index.jsx
@@ -1,7 +1,105 @@
-export default function FormEpi() {
+"use client";
+
+import { Button, Container, Stack, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+
+export default function FormEpi({ epi }) {
+  const [values, setValues] = useState({
+    id: "",
+    name: "",
+    description: "",
+    validity: "",
+  });
+
+  useEffect(() => {
+    if (epi) {
+      setValues({
+        id: epi.id,
+        name: epi.name,
+        description: epi.description,
+        validity: epi.validity,
+      });
+    }
+  }, [epi]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (epi) {
+      try {
+        const response = await fetch(`http://localhost:9191/epi/${epi.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+        const data = await response.json();
+        console.log(data);
+        alert("EPI atualizado com sucesso");
+      } catch (error) {
+        console.error(error);
+        alert("Erro ao atualizar o EPI");
+      }
+    } else {
+      try {
+        const response = await fetch("http://localhost:9191/epi", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+        const data = await response.json();
+        console.log(data);
+        alert("EPI cadastrado com sucesso");
+      } catch (error) {
+        console.error(error);
+        alert("Erro ao cadastrar o EPI");
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>FormEpi</h1>
-    </div>
+    <>
+      <Container component="form" onSubmit={handleSubmit}>
+        <Stack spacing={2}>
+          <TextField
+            name="name"
+            label="Nome"
+            variant="outlined"
+            fullWidth
+            required
+            value={values.name}
+            onChange={(event) =>
+              setValues({ ...values, name: event.target.value })
+            }
+          />
+          <TextField
+            name="description"
+            label="Descrição"
+            variant="outlined"
+            fullWidth
+            required
+            value={values.description}
+            onChange={(event) =>
+              setValues({ ...values, description: event.target.value })
+            }
+          />
+          <TextField
+            name="validity"
+            label="Validade"
+            variant="outlined"
+            fullWidth
+            value={values.validity}
+            onChange={(event) =>
+              setValues({ ...values, validity: event.target.value })
+            }
+          />
+          <Button variant="contained" fullWidth type="submit">
+            Enviar
+          </Button>
+        </Stack>
+      </Container>
+    </>
   );
 }
