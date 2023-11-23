3d4aaa2742a814ef19b0a44a6f06c518e74318c5
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-11-22 21:55:28 -0400

    Update page edit epi

--- src/app/(logged)/epi/[id]/page.jsx
+++ src/app/(logged)/epi/[id]/page.jsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { FormEpi } from "@/components";
+import { useEffect, useState } from "react";
 
 export default function EditEpi({ params }) {
+  const [dataEpi, setDataEpi] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:9191/epi/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setDataEpi(data))
+      .catch((error) => {
+        console.error(error);
+        alert("Erro ao buscar o EPI");
+      });
+  }, [params.id]);
+
   return (
     <>
       <h1>Editar EPI: {params.id}</h1>
-      <FormEpi />
+      <FormEpi epi={dataEpi} />
     </>
   );
 }
