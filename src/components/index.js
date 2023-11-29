ce164e998ff88a9532b2988ec68f5daadbfbf3ee
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-11-29 11:46:33 -0400

    Change name component card and export logo

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
-export { default as MyCard } from "./MyCard";
+export { default as MyCardEpi } from "./MyCardEpi";
 export { default as MySearch } from "./MySearch";
 export { default as FormEpi } from "./FormEpi";
+export { default as Logo } from "./Logo";
