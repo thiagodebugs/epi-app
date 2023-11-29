683946edd5b4d3569824a344e824bd05376b0f14
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-11-29 16:08:42 -0400

    Export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export { default as Navbar } from "./Navbar";
 export { default as MyCardEpi } from "./MyCardEpi";
+export { default as MyCardUser } from "./MyCardUser";
 export { default as MySearch } from "./MySearch";
 export { default as FormEpi } from "./FormEpi";
+export { default as FormUser } from "./FormUser";
 export { default as Logo } from "./Logo";
